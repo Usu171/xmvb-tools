be3e6178d59f417a47dc023007129315a46c6623
Usu171 <usu171@proton.me>
2026-04-09 16:48:18 +0800

    chore: rm numpy

--- src/xmvb_tools/reorder.py
+++ src/xmvb_tools/reorder.py
@@ -13,9 +13,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import numpy as np
-
-
 REORDER_PERMUTATIONS = {
     # new index -> old index
     # D shell: XX, XY, XZ, YY, YZ, ZZ
